4f663c8b2f16e8cd3e0ee47f6b1ee1fded175200
wadikur <wadikur@gmail.com>
2019-10-04 14:16:32 +0530

    Done with harmonic oscilator2

---
+++ diffeqn/firstorder/euler.f90
@@ -0,0 +1,29 @@
+program Euler
+      implicit none
+      
+      real*8::x_i,y_i,x,y,fxy,h
+      integer::i,num_iteration
+
+
+
+      x_i=0.0d0
+      y_i=0.0d0
+
+      h=0.001
+
+      num_iteration=int(1.55/h)
+
+      open(unit=21,file="Euler_h_0_001.dat")
+      y=y_i
+      x=x_i
+      write(21,*)x,y
+      do i=0,num_iteration
+        x=x+h
+        fxy=1+y*y
+        y=y+h*fxy
+
+        write(21,*)x,y
+      end do
+      print*,x,y-tan(1.55d0)
+      close(21)
+end program Euler
---
+++ diffeqn/firstorder/modeuler.f90
@@ -0,0 +1,33 @@
+program modEuler
+      implicit none
+
+      real*8::x_i,y_i,x,y,fxy,h,f_temp,y_temp
+      integer::i,num_iteration
+
+
+
+      x_i=0.0d0
+      y_i=0.0d0
+
+      h=0.001
+
+      num_iteration=int(1.55/h)
+
+      open(unit=21,file="modifiedEuler_h_0_001.dat")
+      y=y_i
+      x=x_i
+      write(21,*)x,y
+
+      do i=0,num_iteration
+        fxy=1+y*y
+        y_temp=y+(h/2.0d0)*fxy
+        f_temp=1+y_temp*y_temp
+
+        y=y+h*f_temp
+        x=x+h
+
+        write(21,*)x,y
+      end do
+      print*,x,y-tan(1.55d0)
+      close(21)
+end program modEuler
