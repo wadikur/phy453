12b3b347ec4b766bdbe8b02d3f4c69b8741f4a58
wadikur <wadikur@gmail.com>
2019-11-19 16:08:00 +0530

    Done with thermostate and all

--- MD/mdmain.f90
+++ MD/mdmain.f90
@@ -1,11 +1,10 @@
 module md_module
   implicit none
-  integer,parameter:: npart= 1200, niter_eq=10000, niter=10000, n_calc_av=10
-  real*8, parameter:: mass=1.0d0, temp= 1.0d0, sigma=1.0d0,eps=1.0d0,dt=0.005d0
+  integer,parameter:: npart= 1200, niter_eq=2000, niter=10000, n_calc_av=10
+  real*8, parameter:: mass=1.0d0, temp= 1.0d0, sigma=1.0d0,eps=1.0d0
   integer,parameter :: lx=20,ly=20,lz=20
   real*8,parameter:: llx=dfloat(lx), lly=dfloat(ly), llz=dfloat(lz),npartf=dfloat(npart)
   real*8,parameter:: llxby2=llx/2.0d0,llyby2=lly/2.0d0,llzby2=llz/2.0d0
-  real*8,parameter:: dt2by2bym=(0.50d0*dt**2)/mass,dtby2bym=0.50d0*dt/mass
   real*8,parameter:: rc=2.50d0*sigma
   real*8,parameter:: sigma6=sigma**6,sigma12=sigma**12,epst4=4*eps
   real*8,parameter:: fc=epst4*((12.0d0*sigma12/(rc**13))-(6.0d0*sigma6/(rc**7)))
@@ -92,7 +91,13 @@ contains
 
 
   !updating the position
-  subroutine update_pos()
+  subroutine update_pos(dt)
+    implicit none
+    real*8,intent(in)::dt
+    real*8:: dt2by2bym,dtby2bym
+    dt2by2bym=(0.5d0*dt**2)/mass
+    dtby2bym=(0.5d0*dt)/mass
+
     do i=1,npart
        pos(3*i-2)=pos(3*i-2)+vel(3*i-2)*dt+dt2by2bym*force(3*i-2)
        pos(3*i-1)=pos(3*i-1)+vel(3*i-1)*dt+dt2by2bym*force(3*i-1)
@@ -104,7 +109,12 @@ contains
     end do
   end subroutine update_pos
   !Update the velocity
-  subroutine update_vel()
+  subroutine update_vel(dt)
+    implicit none
+    real*8,intent(in)::dt
+    real*8:: dt2by2bym,dtby2bym
+    dt2by2bym=(0.5d0*dt**2)/mass
+    dtby2bym=(0.5d0*dt)/mass
     kinetic_energy=0.0d0
     av_vx=0.0d0
     av_vy=0.0d0
@@ -155,6 +165,22 @@ contains
     end do
     potential_energy=potential_energy/npartf
   end subroutine calc_force
+  subroutine thermostate(step)
+    implicit none
+    integer,intent(in):: step
+    real*8:: theory_ke, scalef
+
+    if (mod(step,200)==0) then
+       theory_ke=1.50d0*temp
+       scalef=dsqrt(theory_ke/kinetic_energy)
+       vel=vel*scalef
+       kinetic_energy=0.0d0
+       do i=1,npart
+          kinetic_energy=kinetic_energy+vel(3*i-2)*vel(3*i-2)+vel(3*i-1)*vel(3*i-1)+vel(3*i)*vel(3*i)
+       end do
+       kinetic_energy=(0.5d0*mass*kinetic_energy)/npartf
+    end if
+  end subroutine thermostate
 
 
 end module md_module
@@ -163,52 +189,118 @@ end module md_module
 program MD
   use md_module
   implicit none
-  integer:: time
+  integer:: time,steps=200
+  real*8:: initial_pos(3*npart),initial_vel(3*npart),initial_force(3*npart),initial_old_force(3*npart)
+  real*8::delta_t
+  open(31,file="energy_005.dat")
+  open(32,file="kinetic_energy_005.dat")
+  open(33,file="potential_energy_005.dat")
+  open(37,file="energy_01.dat")
+  open(38,file="kinetic_energy_01.dat")
+  open(39,file="potential_energy_01.dat")
+  open(40,file="energy_0025.dat")
+  open(41,file="kinetic_energy_0025.dat")
+  open(42,file="potential_energy_0025.dat")
 
-  open(31,file="energy.dat")
-  open(32,file="kinetic_energy.dat")
-  open(33,file="potential_energy.dat")
-  open(34,file="potential_energy_eq.dat")
-  open(35,file="kinetic_energy_eq.dat")
-  open(36,file="energy_eq.dat")
+  ! open(34,file="potential_energy_eq.dat")
+  ! open(35,file="kinetic_energy_eq.dat")
+  ! open(36,file="energy_eq.dat")
   print*,"Initializing positions"
   call init_pos()
   print*, "Positions initialized"
-  call calc_force()
   print*,"Initializing velocity"
   call init_vel()
+  print*, "Velocity initialized"
   print*,"Initial Kinetic Energy",kinetic_energy
   print*,"initial Potential Energy",potential_energy
 
-  print*, "Velocity initialized"
+  delta_t=0.005d0
   do time=1,niter_eq
      write(36,*) time,kinetic_energy+potential_energy
      write(35,*) time,kinetic_energy
      write(34,*) time,potential_energy
-     call update_pos()
+     call update_pos(delta_t)
      call calc_force()
-     call update_vel()
+     call update_vel(delta_t)
      print*,"Equilibrium steps going on",time
   end do
   close(34)
   close(35)
   close(36)
   print*,"Equilibrium done"
-  do time=niter_eq+1,niter+niter_eq
-     write(31,*) time,kinetic_energy+potential_energy
-     write(32,*) time,kinetic_energy
-     write(33,*) time,potential_energy
-     print*, "No_of_iteration>",time
+  initial_pos=pos
+  initial_vel=vel
+  initial_old_force=old_force
+  initial_force=force
+  delta_t=0.005d0
+  pos=initial_pos
+  vel=initial_vel
+  old_force=initial_old_force
+  force=initial_force
+  do time=1,niter
+     print*,delta_t, "No_of_iteration>",time
      ! print*, "momentum along x",av_vx
      ! print*, "momentum along y",av_vy
      ! print*, "momentum along z",av_vz
-     call update_pos()
+     call update_pos(delta_t)
      call calc_force()
-     call update_vel()
+     call update_vel(delta_t)
+     call thermostate(steps)
      ! if (mod(time,n_calc_av)==0) call calc_thermodyn
+     write(31,*) time,kinetic_energy+potential_energy
+     write(32,*) time,kinetic_energy
+     write(33,*) time,potential_energy
+
   end do
   close(31)
   close(32)
   close(33)
+
+  pos=initial_pos
+  vel=initial_vel
+  force=initial_force
+  old_force=initial_old_force
+  delta_t=0.01d0
+
+  do time=1,niter
+     print*,delta_t, "No_of_iteration>",time
+     ! print*, "momentum along x",av_vx
+     ! print*, "momentum along y",av_vy
+     ! print*, "momentum along z",av_vz
+     call update_pos(delta_t)
+     call calc_force()
+     call update_vel(delta_t)
+     call thermostate(steps)
+     ! if (mod(time,n_calc_av)==0) call calc_thermodyn
+     write(37,*) time,kinetic_energy+potential_energy
+     write(38,*) time,kinetic_energy
+     write(39,*) time,potential_energy
+
+  end do
+  close(37)
+  close(38)
+  close(39)
+  pos=initial_pos
+  vel=initial_vel
+  force=initial_force
+  old_force=initial_old_force
+  delta_t=0.0025d0
+  do time=1,niter
+     print*,delta_t, "No_of_iteration>",time
+     ! print*, "momentum along x",av_vx
+     ! print*, "momentum along y",av_vy
+     ! print*, "momentum along z",av_vz
+     call update_pos(delta_t)
+     call calc_force()
+     call update_vel(delta_t)
+     call thermostate(steps)
+     write(40,*) time,kinetic_energy+potential_energy
+     write(41,*) time,kinetic_energy
+     write(42,*) time,potential_energy
+    ! if (mod(time,n_calc_av)==0) call calc_thermodyn
+  end do
+  close(40)
+  close(41)
+  close(42)
 
 end program MD
